fd8863b6ac37c4395116144125c99a392e0e1ada
Daniel David <dannieimeh@gmail.com>
2024-01-05 19:00:24 +0100

    fixed mobile view of create order

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -13,11 +13,11 @@ function CartItem({ item }) {
   const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
-    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+    <li className="w-full py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center sm:gap-6">
+      <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
 
         <UpdateItemQuantity
